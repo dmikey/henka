0f13a6971ed9934dbb6644944c826d9552f00746
Derek M. Anderson <derek.anderson@coxinc.com>
2013-09-18 00:26:15 -0500

    initial commit of henka, woot

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+//install closure compiler on OSX
+//brew install closure-compiler
+//run "brew --prefix closure-compiler" to obtain path
+    grunt.initConfig({
+        'closure-compiler': {
+            frontend: {
+                    closurePath: '/usr/local/opt/closure-compiler/libexec',
+                    js: 'src/js/henka-src.js',
+                    jsOutputFile: 'build/henka.min.js',
+                    maxBuffer: 500,
+                    noreport: true,
+                    options: {
+                        compilation_level: 'ADVANCED_OPTIMIZATIONS',
+                        language_in: 'ECMASCRIPT5_STRICT'
+                    }
+                }
+            }
+    });
+
+    grunt.loadNpmTasks('grunt-closure-compiler');
+    
+    grunt.registerInitTask('default', 'run the full build process', [
+        'closure-compiler'
+    ]);
+
+};
