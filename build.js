1304f3e33064f5dc602075d6ed73a303ee1ee5d0
Derek Anderson <derek.anderson@lge.com>
2014-09-23 16:45:27 -0700

    added gunbai to build process, for requireJS updates

--- build.js
+++ build.js
@@ -5,11 +5,14 @@ var lib_name = 'henka';
 var config = {
     baseUrl: 'src',
     name: lib_name,
-    out: 'dist/'+ lib_name + '-min.js'
+    out: 'dist/'+ lib_name + '-min.js',
+    gunbai: 'src/gunbai/dist/gunbai.min.js'
 };
 
 requirejs.optimize(config, function (buildResponse) {
 
+    //this appends turns the lib def into a returnable RequireJS
+    //module, also used by gunbai to execute standalone
     var appends = [
         'define(["',
             lib_name,
@@ -20,9 +23,20 @@ requirejs.optimize(config, function (buildResponse) {
         '; });'
     ];
 
+    //append gunbai to henka to add standalone capabilities
     fs.appendFile(config.out, appends.join(''), function (err) {
       if (err) throw err;
-      console.log(lib_name + ' has been built!');
+
+        var out = [
+            config.gunbai,
+            config.out
+        ].map(function(filePath){
+            return fs.readFileSync(filePath, 'utf-8');
+        });
+
+        fs.writeFileSync(config.out, out.join(''), 'utf-8');
+
+        console.log(lib_name + ' has been built!');
     });
 
 }, function(err) {
